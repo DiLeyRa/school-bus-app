77fbba5eb678447f22f3dc18a3f4855b2181a3b4
Alterna87 <fvelueta@outlook.com>
2018-10-29 16:31:07 +0000

    add login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
+import {AuthService} from './services/auth.service'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyACSAcbXPV35d_foAOsPK7QBjucZFeTVJ4",
+   authDomain: "school-bus-app-96816.firebaseapp.com",
+   databaseURL: "https://school-bus-app-96816.firebaseio.com",
+   projectId: "school-bus-app-96816",
+   storageBucket: "school-bus-app-96816.appspot.com",
+   messagingSenderId: "639218912380"
+  }
 };
