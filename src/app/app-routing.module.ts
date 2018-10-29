6c0cbaf64d721f4cd91de63040dcf2ffd2d91842
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-10-29 15:47:28 -0600

    Agregando el mapa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ubicacion', component:UbicacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/ubicacion/ubicacion.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+
+declare var H: any;
+
+@Component({
+  selector: 'app-ubicacion',
+  templateUrl: './ubicacion.component.html',
+  styleUrls: ['./ubicacion.component.css']
+})
+export class UbicacionComponent implements OnInit {
+  private platform: any;
+
+    @ViewChild("map")
+    public mapElement: ElementRef;
+
+  public constructor() {
+    this.platform = new H.service.Platform({
+        "app_id": "EmLr27hwIitMqx8wppBZ",
+        "app_code": "DNJEPETR0FKisPTrrPBW5g"
+    });
+}
+
+public ngOnInit() { }
+
+public ngAfterViewInit() {
+    let defaultLayers = this.platform.createDefaultLayers();
+    let map = new H.Map(
+        this.mapElement.nativeElement,
+        defaultLayers.normal.map,
+        {
+            zoom: 10,
+            center: { lat: 19.4978, lng:  -99.1269 }
+        }
+    );
+}
+
+}
+
