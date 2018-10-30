446ab2bced92affa2fafdb1313e940d813165679
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-10-30 12:44:56 -0600

    Agregando deloy

--- src/app/components/ubicacion/ubicacion.component.ts
+++ src/app/components/ubicacion/ubicacion.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
-import { AuthService } from '../../services/auth.service';
 
 declare var H: any;
 
@@ -9,42 +8,32 @@ declare var H: any;
   styleUrls: ['./ubicacion.component.css']
 })
 export class UbicacionComponent implements OnInit {
-  private platform: any;
-
-    @ViewChild("map")
-    public mapElement: ElementRef;
-
-  public constructor(
-    public authService: AuthService
-  ) {
-      
-    this.platform = new H.service.Platform({
-        "app_id": "EmLr27hwIitMqx8wppBZ",
-        "app_code": "DNJEPETR0FKisPTrrPBW5g",
-        useHTTPS: true
-    });
-}
-
-public ngOnInit() { }
-
-public ngAfterViewInit() {
-    let pixelRatio = window.devicePixelRatio || 1;
-    let defaultLayers = this.platform.createDefaultLayers({
-        tileSize: pixelRatio === 1 ? 256 : 512,
-        ppi: pixelRatio === 1 ? undefined : 320
-    });
-    let map = new H.Map(
-        this.mapElement.nativeElement,
-        defaultLayers.normal.map, {pixelRatio
-        : pixelRatio});
-
-        let behavior = new H.behavior(new H.ma)
-
-        let ui = H.ui.UI.createDefault(map, defaultLayers);
-
-        this.authService.moveMapBerlin(map);
-     
-}
-
-}
-
+    private platform: any;
+  
+      @ViewChild("map")
+      public mapElement: ElementRef;
+  
+    public constructor() {
+      this.platform = new H.service.Platform({
+          "app_id": "EmLr27hwIitMqx8wppBZ",
+          "app_code": "DNJEPETR0FKisPTrrPBW5g"
+      });
+  }
+  
+  public ngOnInit() { }
+  
+  public ngAfterViewInit() {
+      let defaultLayers = this.platform.createDefaultLayers();
+      let map = new H.Map(
+          this.mapElement.nativeElement,
+          defaultLayers.normal.map,
+          {
+              zoom: 10,
+              center: { lat: 19.4978, lng:  -99.1269 }
+          }
+      );
+  }
+  
+  }
+  
+  
